a163a3ac2675cbd6f8392cf44f0f9bd0cadb0b41
Sampsa Kares <sampsa.kares@pp.inet.fi>
2018-03-27 16:54:43 +0300

    search update

--- app_server/controllers/search.js
+++ app_server/controllers/search.js
@@ -1,27 +1,33 @@
-const search = function(req, res){
-    res.render('search', { title: 'Express' });
-}
+const request = require('request');
+const apiURL = require('./apiURLs');
 
-// fetch('https://kares-sampsa.azurewebsites.net/api/word')
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(word) {
-//     console.log(word);
-//     console.log(word[0].word);
+const wordlist = function(req, res) {
+    const path = '/api/word';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
 
-// })
-
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var myObj = JSON.parse(this.responseText);
-//         document.getElementById("demo").innerHTML = myObj.name;
-//     }
-// };
-// xmlhttp.open("GET", "https://kares-sampsa.azurewebsites.net/api/word", true);
-// xmlhttp.send();
+    request(
+        requestOptions,
+        function (err, response, body){
+            if (err) {
+                res.render('error', {message: err.message});
+            } else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' + response.statusMessage + ' ('+ response.statusCode + ')' });
+            } else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            } else if (!body.length) {
+                res.render('error', {message: 'No documents in collection'});
+            } else {
+                res.render('search', { title: 'Express', words: body});
+            }
+        }
+    )
+};
 
 module.exports = {
-    search
+    wordlist, search
 };
